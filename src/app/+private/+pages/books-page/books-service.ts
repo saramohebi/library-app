600072c5c0126a799ff5a7f12b0c97fd8af56da7
saramohebi <moheb.sara85@gmail.com>
2025-12-13 01:37:24 -0800

    add items

--- src/app/+private/+pages/books-page/books-service.ts
+++ src/app/+private/+pages/books-page/books-service.ts
@@ -5,7 +5,12 @@ import { BookItem } from './books-page';
   providedIn: 'root',
 })
 export class BooksService {
-   private data: BookItem[] = [{ id: 1, title: 'طراحی وب', writer: 'رضا بهرامی', publisher: 'محمود حسینی', price: 12000 }];
+   private data: BookItem[] = [{ id: 1, title: 'طراحی وب', writer: 'رضا بهرامی', publisher: 'محمود حسینی', price: 12000 },
+      { id: 2, title: 'پایگاه داده  ', writer: 'مریم نظری', publisher: 'محمود حسینی', price: 8000 },
+      { id: 3, title: 'ریاضیات ', writer: 'علی مهربانی', publisher: 'مهدی الماسی', price: 5000 },
+      { id: 4, title: 'مبانی امنیت شبکه ', writer: 'احمدرضا ناجی', publisher: 'پرستو یحیایی', price: 10000 },
+      { id: 5, title: 'ساختمان گسسته', writer: 'آرزو فرجی', publisher: 'زیبا مقدسی', price: 7500 }
+   ];
    add(book:BookItem) { //Create
     this.data.push(book);
 }
--- src/app/+private/+pages/members-page/members-service.ts
+++ src/app/+private/+pages/members-page/members-service.ts
@@ -5,7 +5,12 @@ import { MemberItem } from './members-page';
   providedIn: 'root',
 })
 export class MembersService {
-     private users: MemberItem[] = [{ id: 1, name: 'سارا ', family: 'شمسی محب', bookname: 'طراحی وب', price: 12000 ,date:'azar',status:'پرداخت شده'}];
+     private users: MemberItem[] = [{ id: 1, name: 'سارا ', family: 'شمسی محب', bookname: 'طراحی وب', price: 12000 ,date:'آبان ماه ',status:'پرداخت شده'},
+      { id: 1, name: 'کیمیا', family: 'عبدی', bookname: 'ریاضیات', price: 5000 ,date:'خرداد',status:'پرداخت نشده'},
+      { id: 1, name: 'حانیه', family: 'ابراهیمی', bookname: 'ساختمان گسسته', price: 7500 ,date:'مرداد',status:'پرداخت نشده'},
+      { id: 1, name: 'رها', family: 'یوسفی', bookname: 'پایگاه داده ', price: 8000 ,date:'دی',status:'پرداخت شده'},
+
+     ];
      add(users:MemberItem) { //Create
       this.users.push(users);
   }
